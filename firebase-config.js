8a236dd5d27773f4510e9446e91250dfa230dd05
MAAZ3319 <maaztalha33@gmail.com>
2025-07-28 13:58:04 +0530

    deployed

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDjxNh-cjbhzixz8yWwBCUPQAX2z5QV2WU",
+  authDomain: "maaz-talha.firebaseapp.com",
+  projectId: "maaz-talha",
+  storageBucket: "maaz-talha.firebasestorage.app",
+  messagingSenderId: "523471134384",
+  appId: "1:523471134384:web:5c17cc3e0bc9b97fcde575"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
